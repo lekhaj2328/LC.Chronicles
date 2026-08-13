db1ae1143340e1cd913fb539b7acc9db0a20c509
lekhaj2328 <lekhaj222@gmail.com>
2026-08-13 20:12:59 +0530

    Time: 4 ms (17.37%), Space: 43.9 MB (30.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,19 +1,9 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        Map<Integer,Integer> s=new TreeMap<>();
-        for(int a:nums2){
-            s.put(a,s.getOrDefault(a,0)+1);
+        int ind=m;
+        for(int i=0;i<n;i++){
+            nums1[ind++]=nums2[i];
         }
-        for(int i=0;i<m;i++){
-            s.put(nums1[i],s.getOrDefault(nums1[i],0)+1);
-        }
-        int ind=0;
-        for(int a:s.keySet()){
-            for(int i=0;i<s.get(a);i++){
-                nums1[ind++]=a;
-            }
-        }
-       
-        
+        Arrays.sort(nums1);
     }
 }
