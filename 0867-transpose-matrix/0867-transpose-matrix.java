8a84e88af5db64dafd7e92664a2318809f2bd993
lekhaj2328 <lekhaj222@gmail.com>
2026-08-13 22:50:37 +0530

    Time: 1 ms (26.3%), Space: 46.7 MB (49.13%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.java
+++ 0867-transpose-matrix/0867-transpose-matrix.java
@@ -4,15 +4,10 @@ class Solution {
         List<Integer> l=new ArrayList<>();
         for(int i=0;i<m.length;i++){
             for(int j=0;j<m[0].length;j++){
-                l.add(m[i][j]);
+                tm[j][i]=m[i][j];
             }
         }
-        int ind=0;
-        for(int j=0;j<tm[0].length;j++){
-            for(int i=0;i<tm.length;i++){
-                tm[i][j]=l.get(ind++);
-            }
-        }
+        
         return tm;
     }
 }
